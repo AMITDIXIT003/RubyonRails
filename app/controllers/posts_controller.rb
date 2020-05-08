ca18582bd5469607a5d77a701c7d842b9348e17b
AMITDIXIT003 <amitdixit003@gmail.com>
2020-05-09 01:48:34 +0530

    db changes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+  
+  def index
+    @title = 'Blog Posts'
+    @content_first = 'This is sample text for sample bolg 1'
+  end
+  
+  def new 
+  
+  end
+
+  def create
+  
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+  
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+
+end
