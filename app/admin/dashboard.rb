c8a6cec2fbba231fdcfc226fb234ac09304dac15
AMITDIXIT003 <amitdixit003@gmail.com>
2020-05-13 18:24:23 +0530

    commit

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,12 +2,12 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+#    div class: "blank_slate_container", id: "dashboard_default_message" do
+#      span class: "blank_slate" do
+#        span I18n.t("active_admin.dashboard_welcome.welcome")
+#        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+#      end
+#    end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
@@ -19,11 +19,32 @@ ActiveAdmin.register_page "Dashboard" do
 	     link_to post.title,[:admin,post]
 	   end
        column :category,:sortable => :category
-	   column :crated_at
+	   column :created_at
 	 end
 	 strong (link_to "Show all Posts",:admin_posts)
     end
     
+#categories
+
+     section "Categories", :priority => 2 do
+       table_for Category.order("id desc").limit(15) do
+       column :id
+	   column :"Blog Category",:name do |category|
+	     link_to category.name,[:admin,category]
+	   end
+       column :name,:sortable => :name
+	   column :created_at
+	 end
+	 strong (link_to "Show all Blog Categories",:admin_categories)
+     end 
+
+
+   # index as: :blog do
+   #   title do |post|
+   #     span post.title,      class: 'title'
+   #     span post.created_at, class: 'created_at'
+   #   end
+   # end
 
     #   column do
     #     panel "Info" do
