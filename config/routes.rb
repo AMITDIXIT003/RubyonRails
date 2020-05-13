3c37290cf16ce65ab79b0f3026fa4a687103ebc6
AMITDIXIT003 <amitdixit003@gmail.com>
2020-05-14 01:07:32 +0530

    Commit static page menu changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'home/index'
   resources :posts
   resources :categories
   root to: "home#index"
+
+  get '/about', :to => 'pages#about'
+  get '/resources', :to => 'pages#resources'
+  get '/contact', :to => 'pages#contact'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
